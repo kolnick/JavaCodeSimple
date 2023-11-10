d96e2c5aa9a0cfa0d390f8c4986fbcb8df8bd7a4
caochaojie <258920553@qq.com>
2023-11-10 23:16:21 +0800

    jproto test

--- java-code-simple-parent/framework/serializable/src/test/java/SerializationTest.java
+++ java-code-simple-parent/framework/serializable/src/test/java/SerializationTest.java
@@ -1,11 +1,18 @@
+import com.baidu.bjf.remoting.protobuf.Codec;
+import com.baidu.bjf.remoting.protobuf.ProtobufProxy;
+import com.beust.jcommander.internal.Maps;
 import com.caochaojie.serializable.Serialization;
 import com.caochaojie.serializable.bean.Person;
 import com.caochaojie.serializable.impl.FastJson;
 import com.caochaojie.serializable.impl.Fst;
 import com.caochaojie.serializable.impl.JdkSerialization;
 import com.caochaojie.serializable.impl.KryoSerialization;
+import com.google.common.collect.Lists;
+import jprotobuf.Cat;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
+
+import java.io.IOException;
 
 /**
  * @author caochaojie
@@ -46,6 +53,26 @@ public class SerializationTest {
         byte[] serializer = serialization.serializer(person);
         Person deserialize = serialization.deserialize(Person.class, serializer);
         log.info(deserialize.toString());
+    }
+
+    @Test
+    public void jproto() {
+        Codec<Cat> simpleTypeCodec = ProtobufProxy
+                .create(Cat.class);
+        Cat cat = new Cat();
+        cat.setCatName("dd");
+        cat.setId(1);
+        cat.setFoodMap(Maps.newHashMap(1, 2, 3, 4));
+        cat.setPropIdList(Lists.newArrayList(1l, 2l, 3l, 4l));
+        try {
+            // 序列化
+            byte[] bb = simpleTypeCodec.encode(cat);
+            // 反序列化
+            Cat uncat = simpleTypeCodec.decode(bb);
+            System.out.println(uncat);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Test
