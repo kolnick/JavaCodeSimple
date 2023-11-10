89a329c274d85c7f1bae067ca05e598f06b787ec
caochaojie <258920553@qq.com>
2023-11-11 00:13:34 +0800

    test: trove memory list

--- java-code-simple-parent/framework/high-performance/memory/src/test/java/com/caochaojie/collection/CollectionTest.java
+++ java-code-simple-parent/framework/high-performance/memory/src/test/java/com/caochaojie/collection/CollectionTest.java
@@ -1,5 +1,8 @@
 package com.caochaojie.collection;
 
+import gnu.trove.list.array.TIntArrayList;
+import gnu.trove.map.TIntObjectMap;
+import gnu.trove.map.hash.TIntObjectHashMap;
 import org.agrona.collections.Int2ObjectHashMap;
 import org.agrona.collections.IntArrayList;
 import org.apache.lucene.util.RamUsageEstimator;
@@ -24,17 +27,19 @@ public class CollectionTest {
     public void listMemory() {
         List<Integer> list = new IntArrayList();
         List<Integer> jdkList = new ArrayList<>();
-
+        TIntArrayList tIntArrayList = new TIntArrayList();
         int size = 100_0000;
         for (int i = 0; i < size; i++) {
             list.add(i);
             jdkList.add(i);
+            tIntArrayList.add(i);
         }
         long objectSize = RamUsageEstimator.sizeOf(list);
         System.out.println(objectSize / 1024);
         long jdkListSize = RamUsageEstimator.sizeOf(jdkList);
         System.out.println(jdkListSize / 1024);
-
+        long tIntArrayListSize = RamUsageEstimator.sizeOf(tIntArrayList);
+        System.out.println(tIntArrayListSize / 1024);
     }
 
     @Test
@@ -42,16 +47,20 @@ public class CollectionTest {
         // 创建一个 MutableIntObjectMap 对象
         Int2ObjectHashMap<Object> map = new Int2ObjectHashMap<>();
         Map<Integer, Object> map2 = new HashMap<>();
+        TIntObjectMap tIntIntHashMap = new TIntObjectHashMap();
         int size = 100_0000;
-        for (int i = 0; i < size; i++) {
+        for (int key = 0; key < size; key++) {
             // 向 Map 中添加键值对
-            map.put(i, "Value " + i);
-            map2.put(i, "Value " + i);
+            String value = "Value " + key;
+            map.put(key, value);
+            map2.put(key, value);
+            tIntIntHashMap.put(key,value);
         }
         long objectSize = RamUsageEstimator.sizeOf(map);
         System.out.println(objectSize / 1024);
         long jdkMapSize = RamUsageEstimator.sizeOf(map2);
         System.out.println(jdkMapSize / 1024);
-
+        long tIntIntHashMapSize = RamUsageEstimator.sizeOf(tIntIntHashMap);
+        System.out.println(tIntIntHashMapSize / 1024);
     }
 }
